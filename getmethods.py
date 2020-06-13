16aa52b33499ec965d9f463106331ccc89f9e280
Marcus <mlsoederberg1@gmail.com>
2020-06-13 21:41:37 +0200

    implemented serialization

--- getmethods.py
+++ getmethods.py
@@ -1,4 +1,8 @@
 from config import db
-from models import Transcript
-def read_all():
-    pass
+from models import *
+import json
+def read():
+    tscript = Transcript.query.first()
+    tscript_schema = TranscriptSchema()
+    the_dump = tscript_schema.dump(tscript)
+    return the_dump.json()
--- models.py
+++ models.py
@@ -1,5 +1,6 @@
-from config import db
-#db.metadata.clear()
+from config import db,ma
+from marshmallow import fields
+db.metadata.clear()
 class Transcript(db.Model):
     __tablename__ = "transcript"
     transcript_id = db.Column(db.String(30), primary_key=True)
@@ -9,3 +10,15 @@ class Transcript(db.Model):
     party = db.Column(db.String(2))
     section = db.Column(db.String(100))
     date = db.Column(db.String(30))
+class TranscriptSchema(ma.Schema):
+    transcript_id = fields.Str()
+    transcript = fields.Str()
+    speaker_id = fields.Str()
+    speaker_title = fields.Str()
+    party = fields.Str()
+    section = fields.Str()
+    date = fields.Str()
+
+    class Meta:
+        model = Transcript
+        sqla_session = db.session
