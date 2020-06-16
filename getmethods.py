75e507503c1fa88a06effdd74fdceac915d10485
Marcus <mlsoederberg1@gmail.com>
2020-06-16 13:12:15 +0200

    working on get methods

--- getmethods.py
+++ getmethods.py
@@ -1,8 +1,32 @@
 from config import db
 from models import *
 import json
-def read():
-    tscript = Transcript.query.first()
-    tscript_schema = TranscriptSchema()
-    the_dump = tscript_schema.dump(tscript)
-    return the_dump
+from flask import abort
+
+
+def read_all():
+    tscript = Transcript.query.all()
+    tscript_schema = TranscriptSchema(many=True)
+    data = tscript_schema.dump(tscript)
+    return data
+
+# exampe speaker id 0888807074321
+def read_speaker_id(speaker_id):
+    tscript = Transcript.query.filter(Transcript.speaker_id == speaker_id)
+
+
+    # check if query is empty
+    if tscript.all():
+        tscript_schema = TranscriptSchema(many=True)
+        data = tscript_schema.dump(tscript)
+        return data
+    else:
+        abort(404, "Speaker id not found")
+def read_party(party):
+    tscript = Transcript.query.filter(Transcript.party == party)
+    if tscript.all():
+        tscript_schema = TranscriptSchema(many=True)
+        data = tscript_schema.dump(tscript)
+        return data
+    else:
+        abort(404, "Nothing found")
