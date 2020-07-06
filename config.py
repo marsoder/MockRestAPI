e423afefedfc8011ca4cbb5d1040552c2d676781
Marcus <mlsoederberg1@gmail.com>
2020-07-06 14:24:19 +0200

    some renaming and writing

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ from flask_marshmallow import Marshmallow
 basedir = "/home/xioahei/Learning/MockRiksdagAPI/"
 
 connex_app = connexion.App(__name__, specification_dir=basedir)
-fapp = connex_app.app
-fapp.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + \
+flask_app = connex_app.app
+flask_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + \
         os.path.join(basedir, 'transcripts.db')
 
-fapp.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-db = SQLAlchemy(fapp)
-ma = Marshmallow(fapp)
+flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+db = SQLAlchemy(flask_app)
+ma = Marshmallow(flask_app)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,3 @@
-from connexion import App
 import os
 import pytest
 import sys
@@ -8,63 +7,66 @@ import json
 # appending necessary otherwise ModuleNotFoundError is raised due to not finding httpmethods
 sys.path.append("..")
 
-SWAGGER_PATH = "/home/xioahei/Learning/MockRiksdagAPI"
+BASE_DIR = "/home/xioahei/Learning/MockRiksdagAPI"
 
-from config import fapp, connex_app
+from config import flask_app, connex_app
 from models import Transcript, TranscriptSchema, db
 connex_app.add_api("ParlSpeeches.yml")
-fapp.config['SQLALCHEMY_DATABASE_URI']  = 'sqlite:////' + SWAGGER_PATH + "/tests/test-db.db"
-
-# reads json from file and converts into Transcript object
-f = open("test-transcript.txt", "r").read()
-js = json.loads(f)
-ts = Transcript().from_dict(js)
+flask_app.config['SQLALCHEMY_DATABASE_URI']  = 'sqlite:////' + BASE_DIR + "/tests/test-db.db"
  
 @pytest.fixture(scope="module")
-def app():
-    with fapp.test_client() as c:
+def app_test():
+    """ yields a test app with a test database that contains one test transcrip where transcript_id = testid, party=S, speaker_id = testspeakerid"""
+ 
+# reads json from file and converts into Transcript object
+    with open("test-transcript.txt", "r") as f:
+        content = f.read()
+        js = json.loads(content)
+        global mock_transcript
+        mock_transcript = Transcript().from_dict(js)
+    with flask_app.test_client() as c:
         db.create_all()
         # adds transcript to database before every test for testing endpoints
-        db.session.add(ts)
+        db.session.add(mock_transcript)
         db.session.commit()
         yield c
         db.drop_all()
 
-def test_transcript(app) -> None:
-    response = app.get("MockAPI/transcript")
+def test_transcript(app_test) -> None:
+    response = app_test.get("MockAPI/transcript")
     assert response.status_code == 200
 
-def test_party(app) -> None:
-    response = app.get("MockAPI/transcript?party=")
+def test_party(app_test) -> None:
+    response = app_test.get("MockAPI/transcript?party=")
     assert response.status_code == 200
-def test_party_correct_party(app):
-    response = app.get("MockAPI/transcript?party=S")
+def test_party_correct_party(app_test):
+    response = app_test.get("MockAPI/transcript?party=S")
     transcript = response.get_json().pop()
     assert transcript.get("party") == "S"
-def test_speaker_id(app):
-    response = app.get("MockAPI/transcript/testspeakerid")
+def test_speaker_id(app_test):
+    response = app_test.get("MockAPI/transcript/testspeakerid")
     assert response.status_code == 200
     
-def test_speaker_id_not_found(app):
-    response = app.get("MockAPI/transcript/nonexistingspeakerid")
+def test_speaker_id_not_found(app_test):
+    response = app_test.get("MockAPI/transcript/nonexistingspeakerid")
     assert response.status_code == 404
     
-def test_post(app):
-
-    ts.transcript_id = "newid"
-    data = TranscriptSchema().dump(ts)
-    response = app.post("MockAPI/transcript", json=data)
+def test_post(app_test):
+    
+    mock_transcript.transcript_id = "newid"
+    data = TranscriptSchema().dump(mock_transcript)
+    response = app_test.post("MockAPI/transcript", json=data)
     assert response.status_code == 200
 
-def test_post_id_already_exists(app):
-    data = TranscriptSchema().dump(ts)
-    response = app.post("MockAPI/transcript", json=data)
+def test_post_id_already_exists(app_test):
+    data = TranscriptSchema().dump(mock_transcript)
+    response = app_test.post("MockAPI/transcript", json=data)
     assert response.status_code == 404
     
-def test_delete(app):
-    response = app.delete("MockAPI/transcript/testid")
+def test_delete(app_test):
+    response = app_test.delete("MockAPI/transcript/testid")
     assert response.status_code == 200
 
-def test_delete_not_existing(app):
-    response = app.delete("MockAPI/transcript/nonexistingid")
+def test_delete_not_existing(app_test):
+    response = app_test.delete("MockAPI/transcript/nonexistingid")
     assert response.status_code == 404
